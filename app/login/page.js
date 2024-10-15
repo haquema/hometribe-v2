c2d3f42fa3e334e7c59329a7b6e0fbdbbf363978
Aziz <113197790+haquema@users.noreply.github.com>
2024-10-15 19:16:14 +0100

    rework login page ui using nextui components

--- app/login/page.js
+++ app/login/page.js
@@ -1,14 +1,23 @@
+import { Button, Input } from '@nextui-org/react'
 import { login, signup } from './actions'
 
 export default function LoginPage() {
+  
+
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
+    <form className='py-2 flex flex-col gap-2 w-96 self-center'>
+      <Input type='email' name='email' label='Email' classNames={{
+        inputWrapper: 'bg-white border border-blue-200',
+        base: '',
+      }}/>
+      <Input type='password' name='password' label='Password' classNames={{
+        inputWrapper: 'bg-white border border-blue-200',
+        base: '',
+      }}/>
+      <div className='flex gap-4'>
+        <Button color='primary' variant='bordered' className='rounded-md w-1/2'>Signup</Button>
+        <Button color='primary' className='rounded-md w-1/2'>Login</Button>
+      </div>
     </form>
   )
 }
