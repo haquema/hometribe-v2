96cdb50168d00383cd8990eef2125be04224ebbb
Aziz <113197790+haquema@users.noreply.github.com>
2025-07-07 20:26:21 +0100

    feat: implement initial user creation upon successful registration

--- backend/services/auth.service.ts
+++ backend/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { userRegistrationSchema, userRegistrationInput } from "../validation/user.validation";
 import prisma from "../utils/prisma";
-import { email } from "zod/v4";
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 
 export class AuthService {
@@ -17,6 +18,30 @@ export class AuthService {
       // return { success: true, message: "If this email is not registered, you will receive a verification email." };
       return { success: false, errors: [{ message: "Email already registered" }] };
     }
+
+    const hashedUserPassword = await bcrypt.hash(userInput.password, 10);
+    const rawToken = crypto.randomBytes(32).toString('hex');
+    const hashedToken = crypto.createHash('sha256').update(rawToken).digest('hex');
+    const emailVerificationTokenExpires = new Date(Date.now() + 1000 * 60 * 60 * 24);
+    
+    try {
+      const createUser = await prisma.user.create({
+        data: {
+          email: userInput.email,
+          hashedPassword: hashedUserPassword,
+          name: userInput.name,
+          emailVerificationToken: hashedToken,
+          emailVerificationExpiresAt: emailVerificationTokenExpires,
+          role: userInput.role,
+        }
+      })
+      return { success: true, message: "Registration successful. Please check your email to verify your account." };
+    } catch (err) {
+      return {err}
+    }
+    
+
+    console.log(`Verify at: http://yourdomain.com/verify?token=${rawToken}&email=${userInput.email}`);
   }
 
   private async hashPassword() {
--- backend/validation/user.validation.ts
+++ backend/validation/user.validation.ts
@@ -4,7 +4,7 @@ export const userRegistrationSchema = z.object({
   email: z.email(),
   password: z.string().min(8).max(12),
   name: z.string(),
-  role: z.enum(["user", "vendor", "admin"])
+  role: z.enum(["USER", "VENDOR"])
 })
 
 export type userRegistrationInput = z.infer<typeof userRegistrationSchema>;
