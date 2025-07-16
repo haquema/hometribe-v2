3084027d6e220497b03d95729be779893b5222c4
Aziz <113197790+haquema@users.noreply.github.com>
2025-07-16 17:27:00 +0100

    add cors to backend app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import prisma from '../utils/prisma';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: 'http://localhost:5173', // allow your frontend origin
+  credentials: true, // if you need to send cookies/auth headers
+}));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
